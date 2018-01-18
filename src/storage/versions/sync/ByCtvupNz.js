a6306be08b564bb0710debcbf73ea7c210a202a9
dessant <git@rmn.space>
2018-01-18 08:10:37 +0200

    feat: enable Local Storage and IndexedDB clearing for Firefox

---
+++ src/storage/versions/sync/ByCtvupNz.js
@@ -0,0 +1,52 @@
+import browser from 'webextension-polyfill';
+
+import {targetEnv} from 'utils/config';
+
+const message = 'Enable localStorage and indexedDB for Firefox';
+
+const revision = 'ByCtvupNz';
+const downRevision = 'BkFsUtt7f';
+
+const storage = browser.storage.sync;
+
+async function upgrade() {
+  const changes = {};
+
+  if (targetEnv === 'firefox') {
+    const {dataTypes} = await storage.get('dataTypes');
+
+    changes.dataTypes = dataTypes.concat('localStorage', 'indexedDB');
+  }
+
+  changes.storageVersion = revision;
+  return storage.set(changes);
+}
+
+async function downgrade() {
+  const changes = {};
+
+  if (targetEnv === 'firefox') {
+    const {dataTypes, disabledDataTypes} = await storage.get([
+      'dataTypes',
+      'disabledDataTypes'
+    ]);
+
+    const unsupportedDataTypes = ['indexedDB', 'localStorage'];
+    changes.dataTypes = dataTypes.filter(
+      x => unsupportedDataTypes.indexOf(x) === -1
+    );
+    changes.disabledDataTypes = disabledDataTypes.filter(
+      x => unsupportedDataTypes.indexOf(x) === -1
+    );
+  }
+
+  changes.storageVersion = downRevision;
+  return storage.set(changes);
+}
+
+module.exports = {
+  message,
+  revision,
+  upgrade,
+  downgrade
+};
